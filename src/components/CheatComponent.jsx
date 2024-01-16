9e9aef46a8f9c4e5e35bf094caeaf1ceb7b7fc86
Francesco <frngastone@gmail.com>
2024-01-16 18:45:22 +0100

    CheatPageComponent optimization

--- src/components/CheatComponent.jsx
+++ src/components/CheatComponent.jsx
@@ -7,71 +7,70 @@ export const CheatComponent = () => {
     const [fakeRandom1_24, setFakeRandom1_24] = useState('');
 
     useEffect(() => {
-        // Check if data already exists in localStorage
-        const storedData = localStorage.getItem('fakeRandom1_4');
-
-        // If no data exists, set initial data and store it in localStorage
-        if (!storedData) {
-            setFakeRandom1_4(0);
-            localStorage.setItem('fakeRandom1_4', 0);
-        } else {
-            // If data exists, set it to the state
-            setFakeRandom1_4(storedData);
-        }
-    }, []); // The empty dependency array ensures the effect runs only on mount
-
-    // Function to handle changes and store data in localStorage
-    const insertFakeRandom1_4 = (event) => {
-        const newData = event.target.value;
-        setFakeRandom1_4(newData);
-        localStorage.setItem('fakeRandom1_4', newData);
-    };
-
-
-
-    useEffect(() => {
-        // Check if data already exists in localStorage
-        const storedData = localStorage.getItem('fakeRandom1_8');
-
-        // If no data exists, set initial data and store it in localStorage
-        if (!storedData) {
-            setFakeRandom1_8(0);
-            localStorage.setItem('fakeRandom1_8', 0);
-        } else {
-            // If data exists, set it to the state
-            setFakeRandom1_8(storedData);
-        }
-    }, []); // The empty dependency array ensures the effect runs only on mount
-
-    // Function to handle changes and store data in localStorage
-    const insertFakeRandom1_8 = (event) => {
-        const newData = event.target.value;
-        setFakeRandom1_8(newData);
-        localStorage.setItem('fakeRandom1_8', newData);
-    };
-
-
-
-    useEffect(() => {
-        // Check if data already exists in localStorage
-        const storedData = localStorage.getItem('fakeRandom1_24');
-
-        // If no data exists, set initial data and store it in localStorage
-        if (!storedData) {
-            setFakeRandom1_24(0);
-            localStorage.setItem('fakeRandom1_24', 0);
-        } else {
-            // If data exists, set it to the state
-            setFakeRandom1_24(storedData);
-        }
-    }, []); // The empty dependency array ensures the effect runs only on mount
+        
+        const localStorageKeys = ['fakeRandom1_4', 'fakeRandom1_8', 'fakeRandom1_24'];
+    
+        // Loop through localStorage keys
+        localStorageKeys.forEach(key => {
+            // Check if data already exists in localStorage
+            const storedData = localStorage.getItem(key);
+    
+            // If no data exists, set initial data and store it in localStorage
+            if (!storedData) {
+                switch (key) {
+                    case 'fakeRandom1_4':
+                        setFakeRandom1_4(0);
+                        localStorage.setItem(key, 0);
+                        break;
+                    case 'fakeRandom1_8':
+                        setFakeRandom1_8(0);
+                        localStorage.setItem(key, 0);
+                        break;
+                    case 'fakeRandom1_24':
+                        setFakeRandom1_24(0);
+                        localStorage.setItem(key, 0);
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                // If data exists, set it to the state
+                switch (key) {
+                    case 'fakeRandom1_4':
+                        setFakeRandom1_4(storedData);
+                        break;
+                    case 'fakeRandom1_8':
+                        setFakeRandom1_8(storedData);
+                        break;
+                    case 'fakeRandom1_24':
+                        setFakeRandom1_24(storedData);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+    }, []);
 
     // Function to handle changes and store data in localStorage
-    const insertFakeRandom1_24 = (event) => {
-        const newData = event.target.value;
-        setFakeRandom1_24(newData);
-        localStorage.setItem('fakeRandom1_24', newData);
-    };
+    const insertFakeRandomInStorage = (fakeableObject, value) => {
+        switch (fakeableObject) {
+          case '1-4':
+            setFakeRandom1_4(value);
+            localStorage.setItem('fakeRandom1_4', value);
+            break;
+          case '1-8':
+            setFakeRandom1_8(value);
+            localStorage.setItem('fakeRandom1_8', value);
+            break;
+          case '1-24':
+            setFakeRandom1_24(value);
+            localStorage.setItem('fakeRandom1_24', value);
+            break;
+          default:
+            break;
+        }
+      };
 
 
     return (
@@ -86,7 +85,7 @@ export const CheatComponent = () => {
                     <input
                         type="text"
                         value={fakeRandom1_4}
-                        onChange={insertFakeRandom1_4}
+                        onChange={e => insertFakeRandomInStorage("1-4", e.target.value)}
                         placeholder="Enter data" />
 
                 </div>
@@ -97,7 +96,7 @@ export const CheatComponent = () => {
                     <input
                         type="text"
                         value={fakeRandom1_8}
-                        onChange={insertFakeRandom1_8}
+                        onChange={e => insertFakeRandomInStorage("1-8", e.target.value)}
                         placeholder="Enter data" />
 
                 </div>
@@ -108,7 +107,7 @@ export const CheatComponent = () => {
                     <input
                         type="text"
                         value={fakeRandom1_24}
-                        onChange={insertFakeRandom1_24}
+                        onChange={e => insertFakeRandomInStorage("1-24", e.target.value)}
                         placeholder="Enter data" />
 
                 </div>
